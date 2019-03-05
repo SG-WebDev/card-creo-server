08f181c24c978c05aa0341f6ec360f237e89d541
SG-WebDev <sgrobelny.contact@gmail.com>
2019-03-05 03:03:17 +0100

    fixes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let authRoutes = require("./routes/auth");
 // set up our express application
 app.use(cors()); // cors enabled
 app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
+app.use(cookieParser('bleb')); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -30,12 +30,18 @@ mongoose.connect(`mongodb://${user}:${pass}@${host}/${dbName}`);
 var db = mongoose.connection;
 
 require('./config/passport')(passport);
-
+const cookieExpirationDate = new Date();
+const cookieExpirationDays = 365;
+cookieExpirationDate.setDate(cookieExpirationDate.getDate() + cookieExpirationDays);
 // required for passport
 app.use(session({
-    secret: 'eminem', // session secret
+    secret: 'bleb', // session secret
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: cookieExpirationDate // use expires instead of maxAge
+    }
 }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var passport = require('passport');
 
 router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/auth/profile',
-    failureRedirect : 'auth/signup'
+    successRedirect : '/auth/check',
+    failureRedirect : '/auth/signup'
 }));
 
 router.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/auth/profile',
-    failureRedirect : 'auth/login'
+    successRedirect : '/auth/check',
+    failureRedirect : '/auth/login'
 }));
 router.get('/check', isLoggedIn, (req, res) => {
     res.status(200).json(req.user);
@@ -17,7 +17,7 @@ router.get('/check', isLoggedIn, (req, res) => {
 router.get('/logout', isLoggedIn, (req, res) => {
     req.logout();
     res.status(200).json({
-        'message': 'successfully logout'
+        'message': 'Successfully Logout'
     });
 });
 
@@ -25,9 +25,12 @@ module.exports = router;
 
 //route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
+    if (req.isAuthenticated()) {
         return next();
-    res.status(400).json({
-        'message': 'access denied'
-    });
+    }
+    else {
+        res.status(200).json({
+            'message': 'Access Denied',
+        });
+    }
 }
